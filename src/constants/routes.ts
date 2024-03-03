4122cb93f005c26c7cb6385b91361de6b4906c7e
muhammadgata <iamgata.me@gmail.com>
2024-03-03 14:10:19 +0700

    feat(auth): setup next auth and prisma

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const PUBLIC_ROUTES: string[] = [];
+
+export const AUTH_ROUTES: string[] = ["/login", "/register"];
+
+export const API_AUTH_PREFIX: string = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT: string = "/";
---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { auth } from "@/auth";
+import {
+  AUTH_ROUTES,
+  PUBLIC_ROUTES,
+  API_AUTH_PREFIX,
+  DEFAULT_LOGIN_REDIRECT,
+} from "@/constants/routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isAuthenticated = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(API_AUTH_PREFIX);
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return;
+
+  if (isAuthRoute) {
+    if (isAuthenticated) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    return;
+  }
+
+  if (!isAuthenticated && !isPublicRoute) {
+    console.log(nextUrl);
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+
+  return;
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
